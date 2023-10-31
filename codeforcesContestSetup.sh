7d9604740f9373b0cd1085f0a09ee5dd800307e0
Omar Bednaoui <obednaou@c4r1p9.1337.ma>
2023-10-31 10:55:35 +0100

    CodeforcesContestSetupAutomation

---
+++ codeforcesContestSetup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Automates the process of creating a Codeforces contest directory
+
+# Define contest directory name
+contest_dir="codeforces_907_div_2"
+
+# Create the contest folder
+mkdir -p "$contest_dir"
+
+# Create C++ problem files (problem_a.cpp through problem_f.cpp)
+for problem in {a..f}; do
+    touch "$contest_dir/problem_$problem.cpp"
+    cat > "$contest_dir/problem_$problem.cpp" <<EOL
+# include <iostream>
+# include <vector>
+# include <algorithm>
+# include <string>
+# include <cmath>
+
+int main()
+{
+
+    return (0);
+}
+
+EOL
+done
+
+# Create a Makefile
+makefile="$contest_dir/Makefile"
+cat > "$makefile" <<EOL
+CPP_FILE := problem_a.cpp
+TEST := test.txt
+CPPFLAGS := -Wall -Wextra -Werror
+
+all:
+    g++ \$(CPPFLAGS) CPP_FILE && ./a.out < \$(TEST)
+EOL
+
+# Create a test file
+test_file="$contest_dir/test.txt"
+touch "$test_file"
+
+echo "Contest directory '$contest_dir' and files created successfully."
---
+++ codeforces_907_div_2/problem_b.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <vector>
+# include <algorithm>
+# include <string>
+# include <cmath>
+
+int main()
+{
+
+    return (0);
+}
+
